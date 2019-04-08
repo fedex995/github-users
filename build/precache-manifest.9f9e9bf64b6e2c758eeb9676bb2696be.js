6af480f248b0681c3f8fbb5ce1509ceab25264dc
federuiz <federicoruiz@sirius.com.ar>
2019-04-08 14:01:58 +0200

    added build

---
+++ build/precache-manifest.9f9e9bf64b6e2c758eeb9676bb2696be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8180851aedfd40ad5435d4e38cb288",
+    "url": "/static/media/arrow-up-DARK.1e818085.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d2f96c6ef47e4332298",
+    "url": "/static/js/main.e0872060.chunk.js"
+  },
+  {
+    "revision": "240f7b6fbb9a1ee0927d",
+    "url": "/static/js/2.d4998465.chunk.js"
+  },
+  {
+    "revision": "7d2f96c6ef47e4332298",
+    "url": "/static/css/main.773788a6.chunk.css"
+  },
+  {
+    "revision": "240f7b6fbb9a1ee0927d",
+    "url": "/static/css/2.095625e1.chunk.css"
+  },
+  {
+    "revision": "9b63edf8c741a8c021db3c6723cbfee5",
+    "url": "/index.html"
+  }
+];
